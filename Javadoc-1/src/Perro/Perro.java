b2805f5424fc45fab2ef3dfabaf4b860398c9e00
migslow <migueljobemine@gmail.com>
2022-05-30 23:07:43 +0200

    Proyectos para repasar la documentacion en javadoc

---
+++ Javadoc-1/src/Perro/Perro.java
@@ -0,0 +1,89 @@
+/**
+ * @author migslow
+ * @version 1.0
+ * Clase Perro que va a representar las caracteristicas que tiene un perro.
+ */
+package Perro;
+
+public class Perro {
+	/**
+	 * Atributos de la clase
+	 */
+	private String nombre;
+	private String color;
+	private int edad;
+
+	/**
+	 * Contructor con 3 parametros
+	 * @param nombre nombre del perro
+	 * @param color color del perro
+	 * @param edad edad del perro
+	 */
+
+	public Perro(String name, String color, int edad) {
+		super();
+		this.nombre = name;
+		this.color = color;
+		this.edad = edad;
+	}
+
+	/**
+	 * Devuelve el nombre del perro
+	 * @return nombre del perro
+	 */
+
+	public String getName() {
+		return nombre;
+	}
+
+	/**
+	 * Modifica el nombre del perro
+	 * @param nombre del perro
+	 */
+
+	public void setName(String name) {
+		this.nombre = name;
+	}
+
+	/**
+	 * Devuelve el color del perro
+	 * @return color del perro
+	 */
+
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * Modifica el color del perro
+	 * @param color del perro
+	 */
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * Devuelve la edad del perro
+	 * @return edad del perro
+	 */
+
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * Modifica la edad del perro
+	 * @param edad del perro
+	 */
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	@Override
+	public String toString() {
+		return "Perro [name=" + nombre + ", color=" + color + ", edad=" + edad + "]";
+	}
+
+}
---
+++ Javadoc-2/src/Concesionario/Coche.java
@@ -0,0 +1,123 @@
+/**
+ * @author migslow
+ * @version 1.0
+ * Clase Coche que muestran las caracteristicas de un coche que esta en un concesionario.
+ */
+package Concesionario;
+
+import java.util.Date;
+
+public class Coche {
+	/**
+	 * Atributos privados de la clase
+	 */
+	private String color;
+	private int cv;
+	private Date anyoAdquisición;
+	private String matricula;
+	private String marca;
+
+	/**
+	 * Constructor con 5 parametros
+	 * @param color color que lleva el coche desde que salio de la fabrica.
+	 * @param cv caballos de fuerza que tiene el coche.
+	 * @param anyoAdquisición año de adquisición que establece la fecha cuando compraron el coche.
+	 * @param matricula matricula que lleva el coche.
+	 * @param marca marca del coche.
+	 */
+	public Coche(String color, int cv, Date anyoAdquisición, String matricula, String marca) {
+		super();
+		this.color = color;
+		this.cv = cv;
+		this.anyoAdquisición = anyoAdquisición;
+		this.matricula = matricula;
+		this.marca = marca;
+	}
+
+	/** 
+	 * Devuelve el color de serie que tiene el coche desde que salio de la fabrica
+	 * @return color color del coche
+	 */
+	public String getColor() {
+		return color;
+	}
+
+	/**
+	 * Modifica el color del coche
+	 * @param color color del coche
+	 */
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	/**
+	 * Devuelve los caballos de fuerza que tiene de serie el coche
+	 * @return cv caballos de fuerza del coche
+	 */
+	public int getCv() {
+		return cv;
+	}
+
+	/**
+	 * Modifica los caballos de fuerza que tiene de serie el coche
+	 * @param cv caballos de fuerza del coche
+	 */
+	public void setCv(int cv) {
+		this.cv = cv;
+	}
+
+	/**
+	 * Devuelve la fecha desde que el duelo del coche lo adquirio
+	 * @return anyoAdquisición fecha de anyoAdquisición de cuando se compro el coche
+	 */
+	public Date getAnyoAdquisición() {
+		return anyoAdquisición;
+	}
+
+	/**
+	 * Modifica la fecha de la adquisición del coche
+	 * @param anyoAdquisición fecha de anyoAdquisición del coche 
+	 */
+	public void setAnyoAdquisición(Date anyoAdquisición) {
+		this.anyoAdquisición = anyoAdquisición;
+	}
+
+	/**
+	 * Devuelve la matricula con la combinación de caracteres alfanumeticos que establece el concesionario a un coche 
+	 * @return matricula matricula que tiene el coche cuando sale del concesionario
+	 */
+	public String getMatricula() {
+		return matricula;
+	}
+
+	/**
+	 * Modifica la matricula con una combinación de caracteres alfanumericos despues de salir del concesionario
+	 * @param matricula matricula que tiene el coche
+	 */
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	/**
+	 * Devuelve la marca de un fabricante que se dedica al sector automovilistico
+	 * @return marca marca del coche
+	 */
+	public String getMarca() {
+		return marca;
+	}
+
+	/**
+	 * Modifica la marca del coche
+	 * @param marca marca del coche
+	 */
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	@Override
+	public String toString() {
+		return "Coche [color=" + color + ", cv=" + cv + ", anyoAdquisición=" + anyoAdquisición + ", matricula="
+				+ matricula + ", marca=" + marca + "]";
+	}
+
+}
