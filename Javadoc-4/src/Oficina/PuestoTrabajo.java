e723bbcc8a82fec0b603270efd8f7aa8e536a89a
migslow <migueljobemine@gmail.com>
2022-05-31 07:30:56 +0200

    Mas repaso Javadoc

---
+++ Javadoc-4/src/Oficina/PuestoTrabajo.java
@@ -0,0 +1,13 @@
+package Oficina;
+
+public class PuestoTrabajo {
+	int horas;
+	Persona trabajador;
+
+	public PuestoTrabajo(int horas, Persona trabajador) {
+		super();
+		this.horas = horas;
+		this.trabajador = trabajador;
+		trabajador.setEdad(35);
+	}
+}
